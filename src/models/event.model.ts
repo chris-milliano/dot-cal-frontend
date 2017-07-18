ffb5cf5d711574af3441963d161ecd5d8aa8038a
Chris Milliano <MillianoC@gmail.com>
2017-07-18 00:54:29 -0500

    FEAT: Hardcode events service as returning an array of of type event

---
+++ src/models/event.model.ts
@@ -0,0 +1,14 @@
+export class Event {
+
+    // Generic Info
+    title: string;
+
+    // Date/ Time Info
+    startEpoch: number;
+    endEpoch: number;
+    multidayEvent: boolean;
+
+    // Location Info
+    locationTitle: string;
+    mapsLink: string;
+}
---
+++ src/services/calendar/events.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from "@angular/core";
+
+import { Event } from "../../models/event.model";
+
+@Injectable()
+export class EventsService {
+
+    // TODO: This needs to come from a DB
+    // Create a fake static JSON object return
+    private static events: Event[] = [
+
+        {
+            title: "Angie\'s birthday 1",
+            startEpoch: 5,
+            endEpoch: 7,
+            multidayEvent: false,
+            locationTitle: "hello place",
+            mapsLink: "http:\/\/google.com"
+        },{
+            title: "Angie\'s birthday 2",
+            startEpoch: 6,
+            endEpoch: 8,
+            multidayEvent: false,
+            locationTitle: "hello other place",
+            mapsLink: "http:\/\/google.com"
+        },{
+            title: "Angie\'s birthday 2",
+            startEpoch: 10,
+            endEpoch: 17,
+            multidayEvent: false,
+            locationTitle: "hello last place",
+            mapsLink: "http:\/\/google.com"
+        },{
+            title: "Angie\'s birthday 0",
+            startEpoch: 1,
+            endEpoch: 3,
+            multidayEvent: false,
+            locationTitle: "hello first place",
+            mapsLink: "http:\/\/google.com"
+        }
+
+    ]
+
+    //
+    public static getEvents() {
+        return this.events;
+    }
+}
