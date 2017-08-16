9628b8a169e3b9bba42de7cbb4819f20b8ff90e6
Chris Milliano <MillianoC@gmail.com>
2017-08-16 00:33:40 -0500

    FEAT: Event data is populated from statically produced evnet array in the event.component.ts

--- src/pages/calendar/calendar.component.ts
+++ src/pages/calendar/calendar.component.ts
@@ -5,6 +5,26 @@ import { go } from "@ngrx/router-store";
 import { AppState } from "../../state/reducers";
 import { CalendarActions } from "./calendar.actions";
 
+
+
+// TODO: This should be moved to an indipendant file
+// Declare Event Model
+export interface Event {
+
+    // Basic info
+    title: string;
+
+    // Time info
+    startTime: number;
+    endTime: number;
+    isAllDay: boolean;
+
+    // Location info
+    locationText: string;
+}
+
+
+
 @Component ({
     templateUrl: "calendar.component.html",
     styleUrls: [ "calendar.component.scss" ]
@@ -12,9 +32,48 @@ import { CalendarActions } from "./calendar.actions";
 
 export class CalendarComponent implements OnInit {
 
-    constructor (private actions: CalendarActions, private store: Store<AppState>) {}
+    // Store API returned data to use in HTML
+    eventsData: Array<Event> = [];
+
+    constructor (
+        private actions: CalendarActions,
+        private store: Store<AppState>
+    ) {}
 
     ngOnInit() {
         console.log("Init calendar.component");
+
+        // TODO: This needs to come from the store or at least a service API
+        // Set eventsData
+        this.eventsData.push({
+            'title': "Brews of the Summer",
+            'startTime': 1503964800000,
+            'endTime': 1503979200000,
+            'isAllDay': false,
+            'locationText': "Schlafly Tap Room Patio"
+        });
+        this.eventsData.push({
+            'title': "Brews of the Summer",
+            'startTime': 1503964800000,
+            'endTime': 1503979200000,
+            'isAllDay': false,
+            'locationText': "Schlafly Tap Room Patio"
+        });
+        this.eventsData.push({
+            'title': "Brews of the Summer",
+            'startTime': 1503964800000,
+            'endTime': 1503979200000,
+            'isAllDay': false,
+            'locationText': "Schlafly Tap Room Patio"
+        });
+        this.eventsData.push({
+            'title': "Brews of the Summer",
+            'startTime': 1503964800000,
+            'endTime': 1503979200000,
+            'isAllDay': false,
+            'locationText': "Schlafly Tap Room Patio"
+        });
+
+
     }
 }
