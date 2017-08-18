75b847db0ccadeef7f5a6febf8f79e6f01daaef1
Chris Milliano <MillianoC@gmail.com>
2017-08-18 00:11:28 -0500

    FEAT: Toggleable event description boxes

--- src/pages/calendar/calendar.component.ts
+++ src/pages/calendar/calendar.component.ts
@@ -12,6 +12,7 @@ import { CalendarActions } from "./calendar.actions";
 export interface Event {
 
     // Basic info
+    id: number;
     title: string;
 
     // Time info
@@ -21,6 +22,10 @@ export interface Event {
 
     // Location info
     locationText: string;
+
+    // Description info
+    descriptionText: string;
+    descriptionIsOpen: boolean;
 }
 
 
@@ -46,34 +51,46 @@ export class CalendarComponent implements OnInit {
         // TODO: This needs to come from the store or at least a service API
         // Set eventsData
         this.eventsData.push({
+            'id': 0,
             'title': "Brews of the Summer",
             'startTime': 1503964800000,
             'endTime': 1503979200000,
             'isAllDay': false,
-            'locationText': "Schlafly Tap Room Patio"
+            'locationText': "Schlafly Tap Room Patio",
+            'descriptionText': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'descriptionIsOpen': false
         });
         this.eventsData.push({
+            'id': 1,
             'title': "Brews of the Summer",
             'startTime': 1503964800000,
             'endTime': 1503979200000,
             'isAllDay': false,
-            'locationText': "Schlafly Tap Room Patio"
+            'locationText': "Schlafly Tap Room Patio",
+            'descriptionText': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'descriptionIsOpen': false
         });
         this.eventsData.push({
+            'id': 2,
             'title': "Brews of the Summer",
             'startTime': 1503964800000,
             'endTime': 1503979200000,
             'isAllDay': false,
-            'locationText': "Schlafly Tap Room Patio"
+            'locationText': "Schlafly Tap Room Patio",
+            'descriptionText': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'descriptionIsOpen': false
         });
         this.eventsData.push({
+            'id': 3,
             'title': "Brews of the Summer",
             'startTime': 1503964800000,
             'endTime': 1503979200000,
             'isAllDay': false,
-            'locationText': "Schlafly Tap Room Patio"
+            'locationText': "Schlafly Tap Room Patio",
+            'descriptionText': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            'descriptionIsOpen': false
         });
-
-
     }
+
+
 }
